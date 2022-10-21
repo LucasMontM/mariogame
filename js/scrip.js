eeffbd8e012824ff6bd8e35a925ebbc175c472e5
Lucas Mont-Mor <lucasmontmro3@gmail.com>
2022-10-21 17:35:20 -0300

    initia commit

---
+++ js/scrip.js
@@ -0,0 +1,37 @@
+
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () =>{
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump')
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition <= 125 && pipePosition > 0 && marioPosition < 280){
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = 'assests/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
+    }
+
+}, 10)
+
+
+document.addEventListener('keydown', jump);
